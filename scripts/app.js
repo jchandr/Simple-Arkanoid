b790e35c070b6af7581ca4076b5f20cbee8d9f01
Jeyabalaji Chandrasekaran <jchan@Jeyabalajis-MacBook-Pro.local>
2017-05-06 17:01:43 -0400

    added start button

--- scripts/app.js
+++ scripts/app.js
@@ -1,57 +1,79 @@
-function makeNewPosition(){
+// function animateDiv(){
+//     var newq = makeNewPosition();
+//     $('#circle').animate({ top: newq[0], left: newq[1] }, function(){
+//       animateDiv();
+//     });
+//
+//     function makeNewPosition(){
+//
+//         // Get viewport dimensions (remove the dimension of the div)
+//         var h = $(window).height() - 75;
+//         var w = $(window).width() - 75;
+//
+//         var nh = Math.floor(Math.random() * h);
+//         var nw = Math.floor(Math.random() * w);
+//         return [nh,nw];
+//
+//     };
+// };
 
-    // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 75;
-    var w = $(window).width() - 75;
+function drop_and_physics() {
+  var init_width = $(window).width()/2 - document.getElementById('circle').style.width;
+  $('#circle').animate({left: init_width}, function(){
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    return [nh,nw];
+  });
 
+  var current_position = init_width;
+
+  function bounce_physics(current_position_param) {
+    var new_current_position = current_position_param + 1;
+    $('#circle').animate({top: new_current_position}, function(){
+      bounce_physics(new_current_position);
+    })
+  };
 };
 
-function bar_move_enable() {
-    var selected = null;
-    var x_pos = 0, x_elem = 0;
+function start_button_hit() {
 
-    // Will be called when user starts dragging an element
-function _drag_init(elem) {
-    // Store the object of the element which needs to be moved
-    selected = elem;
-    x_elem = x_pos - selected.offsetLeft;
+  document.getElementById('start-button').style.visibility = "hidden";
+  drop_and_physics();
+
 }
 
-  // Will be called when user dragging an element
-  function _move_elem(e) {
-    x_pos = document.all ? window.event.clientX : e.pageX;
-    if (selected !== null) {
-        selected.style.left = (x_pos - x_elem) + 'px';
-        selected.style.top = (y_pos - y_elem) + 'px';
-    }
-  }
-
-// Destroy the object when we are done
-function _destroy() {
-    selected = null;
-}
-
-// Bind the functions...
-document.getElementById('bar').onmousedown = function () {
-    _drag_init(this);
-    return false;
-};
-
-document.onmousemove = _move_elem;
-document.onmouseup = _destroy;
-}
+// function bar_move_enable() {
+//     var selected = null;
+//     var x_pos = 0, x_elem = 0;
+//
+//     // Will be called when user starts dragging an element
+//     function _drag_init(elem) {
+//     // Store the object of the element which needs to be moved
+//     selected = elem;
+//     x_elem = x_pos - selected.offsetLeft;
+//     }
+//
+//   // Will be called when user dragging an element
+//   function _move_elem(e) {
+//     x_pos = document.all ? window.event.clientX : e.pageX;
+//     if (selected !== null) {
+//         selected.style.left = (x_pos - x_elem) + 'px';
+//     }
+//   }
+//
+//   // Destroy the object when we are done
+//   function _destroy() {
+//     selected = null;
+//   }
+//
+//   // Bind the functions...
+//   document.getElementById('bar').onmousedown = function () {
+//     _drag_init(this);
+//     return false;
+//   };
+//
+//   document.onmousemove = _move_elem;
+//   document.onmouseup = _destroy;
+// }
 
 function caught() {
   alert("Nice! You caught me !!");
 }
-
-function animateDiv(){
-    var newq = makeNewPosition();
-    $('#circle').animate({ top: newq[0], left: newq[1] }, function(){
-      animateDiv();
-    });
-};
